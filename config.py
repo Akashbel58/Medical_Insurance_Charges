3508b9f68777a2158b0e5b390c15c23965bc2c11
Akashbel58 <playtrigger58@gmail.com>
2025-07-03 02:52:01 +0530

    add remaining files

---
+++ config.py
@@ -0,0 +1,16 @@
+
+# File_path for .pkl and .json files
+
+CSV_FILEPATH = r'C:\Users\akash\Data_Science_Class_Notebooks\Projects\PythonEnv\medical_insurance_LR\data\medical_insurance.csv'
+
+MODEL_PATH = r'artifacts\lr_model.pkl'
+FEATURE_PATH = r'artifacts\feature.json'
+LABEL_ENC_DATAPATH = r'artifacts\label_enc_data.json'
+
+
+# Database names
+MONGO_URL = 'mongodb://localhost:27017/'
+
+DB_NAME = 'medins_db'
+USER_CRED_COLLECTION_NAME = 'user_details'
+TESTING_COLLECTION_NAME = 'testing_data'
